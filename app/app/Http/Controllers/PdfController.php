86a3a1b91f3177d2d68a3bca003b0e18d7584472
Graham <graham@directonlijn.nl>
2017-06-15 18:19:01 +0200

    changes

--- app/app/Http/Controllers/PdfController.php
+++ app/app/Http/Controllers/PdfController.php
@@ -537,7 +537,7 @@ class PdfController extends Controller
             // sleep(1);
             // dd($nieuwe_factuur);
             $emailData = array(
-                'template' => "hippiemarkt-aalsmeer",
+                'template' => "hippiemarkt-aalsmeer-factuur",
                 'email' => $nieuwe_factuur['email'],
                 'pathToPdf' => $path,
                 'pathToTerms' => $pathToAlgemeneVoorwaarden
