753a8e6059ece5a4501dfe69fda8725ca8f36b0d
Lorrena Lorrena Graham <grahamneal1991@gmail.com>
2018-04-08 23:07:06 +0200

    fixed issue

--- app/config/mail.php
+++ app/config/mail.php
@@ -121,12 +121,4 @@ return [
 
     'pretend' => false,
 
-    'stream' => [
-        'ssl' => [
-            'allow_self_signed' => true,
-            'verify_peer' => false,
-            'verify_peer_name' => false,
-        ],
-    ],
-
 ];
