de202bf6833092a4ac3713b8f8da355099388142
Lorrena Lorrena Graham <grahamneal1991@gmail.com>
2018-04-09 23:45:59 +0200

    lentebraderie bug fix

--- app/resources/views/domains/lentebraderieamsterdam/index.blade.php
+++ app/resources/views/domains/lentebraderieamsterdam/index.blade.php
@@ -7,7 +7,7 @@
 
         <title>Direct Events - Lente braderie Amsterdam</title>
 
-        <base href="http://lentebraderieamsterdam.com/lentebraderieamsterdam/">
+        <base href="http://lentebraderieamsterdam.nl/lentebraderieamsterdam/">
 
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.1/jquery.fancybox.min.css" />
