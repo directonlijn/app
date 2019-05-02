de7bf7754126cdf5e1d6c1db6d7b1f5aa30aca49
Graham Neal <graham@responseconcepts.com>
2019-05-02 21:40:59 +0100

    add

--- app/resources/views/emails/default-factuur.blade.php
+++ app/resources/views/emails/default-factuur.blade.php
@@ -3,7 +3,7 @@
 //    $standhouderExtra = $standhouder['koppelStandhoudersMarktenWithMarkt'];
 //    $markt = $standhouder['koppelStandhoudersMarktenWithMarkt']['Markt'];
 
-    $headerImage = 'http://app.directevents.test/'.strtolower ($markt->Naam).'/mail.jpg';
+    $headerImage = 'http://app.directevents.nl/'.strtolower ($markt->Naam).'/mail.jpg';
 
 ?>
 
@@ -445,7 +445,7 @@
 
                         <td valign="top" class="mcnTextContent" style="padding-top: 0;padding-left: 18px;padding-bottom: 9px;padding-right: 18px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;word-break: break-word;color: #656565;font-family: Helvetica;font-size: 12px;line-height: 150%;text-align: left;">
 
-                            <a href="http://app.directevents.test/mail/view/{{ $markt['factuur-mail-template'] }}/{{ $markt->id }}" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">Mail niet leesbaar of graag in uw browser openen? Klik hier.</a>
+                            <a href="http://app.directevents.nl/mail/view/{{ $markt['factuur-mail-template'] }}/{{ $markt->id }}" target="_blank" style="mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #656565;font-weight: normal;text-decoration: underline;">Mail niet leesbaar of graag in uw browser openen? Klik hier.</a>
                         </td>
                     </tr>
                 </tbody></table>
