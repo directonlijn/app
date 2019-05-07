c50a6d6459a72a86eb2c0c7206a0130de91d3365
Graham Neal <graham@responseconcepts.com>
2019-05-07 20:15:05 +0100

    added reinalda park

--- app/app/Http/routes.php
+++ app/app/Http/routes.php
@@ -26,6 +26,7 @@ Route::group(['domain' => 'www.{domain}.{tld}'], function ($domain) {
     Route::get("mail/view/{slug}/{markt_id?}/{standhouder_id?}", 'MailController@viewTemplate');
 
     Route::get('/', function ($domain) {
+        dd($domain);
         $markt = \App\Models\Markt::where('Website', 'like', '%' . $domain . '%')->orderBy('id', 'desc')->first();
         return view('domains.'.$domain.'.index');
     });
@@ -39,6 +40,7 @@ Route::group(['domain' => '{domain}.{tld}'], function ($domain) {
     Route::get("mail/view/{slug}/{markt_id?}/{standhouder_id?}", 'MailController@viewTemplate');
 
     Route::get('/', function ($domain) {
+        dd($domain);
         $markt = \App\Models\Markt::where('Website', 'like', '%' . $domain . '%')->orderBy('id', 'desc')->first();
         return view('domains.'.$domain.'.index')->with(compact('markt'));
     });
