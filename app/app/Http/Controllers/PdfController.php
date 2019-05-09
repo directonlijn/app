b77bf75cf81c465e3ec68e5fe0dda714dca057ce
Graham Neal <graham@responseconcepts.com>
2019-05-09 12:20:21 +0100

    try to fix dirname pfd download

--- app/app/Http/Controllers/PdfController.php
+++ app/app/Http/Controllers/PdfController.php
@@ -64,7 +64,7 @@ class PdfController extends Controller
 
             $date = \DateTime::createFromFormat("Y-m-d", $factuur->datum);
 
-            $path = getcwd() . "/../public/pdf/".$date->format("Y")."/".$factuur->factuurnummer.".pdf";
+            $path = getcwd() . "/../../app/public/pdf/".$date->format("Y")."/".$factuur->factuurnummer.".pdf";
 
             if (file_exists($path))
             {
