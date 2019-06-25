c5176599fb5b7d0fb260aa9861dcee918899bf49
Graham Neal <graham@responseconcepts.com>
2019-06-25 19:36:29 +0100

    changed default welcome email

--- app/resources/views/emails/default-welcome.blade.php
+++ app/resources/views/emails/default-welcome.blade.php
@@ -512,6 +512,7 @@
 
                         <td valign="top" class="mcnTextContent" style="padding-top: 0;padding-right: 18px;padding-bottom: 9px;padding-left: 18px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;word-break: break-word;color: #202020;font-family: Helvetica;font-size: 16px;line-height: 150%;text-align: left;">
 
+                            <strong>Iedereen die zich aanmeld krijgt deze e-mail. Dit houdt niet in dat u plek heeft op de markt!</strong><br><br>
                             <h1 style="display: block;margin: 0;padding: 0;color: #202020;font-family: Helvetica;font-size: 26px;font-style: normal;font-weight: bold;line-height: 125%;letter-spacing: normal;text-align: left;">Beste standhouder</h1>
 
 <p style="margin: 10px 0;padding: 0;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;color: #202020;font-family: Helvetica;font-size: 16px;line-height: 150%;text-align: left;">
